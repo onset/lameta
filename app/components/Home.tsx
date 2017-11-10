295ac8874d3f4b33b6450f780588b50da8ac112d
John Hatton <hattonjohn@gmail.com>
2017-11-10 16:54:43 -0700

    Add react-tabs

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 let styles = require('./Home.scss');
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
+      <div className={styles.container} data-tid="container">
+        <Tabs >
+          <TabList>
+            {/* <Tab><MdFolder size={25} className="projectTab" />Project</Tab>
+    <Tab><FaComments size={25} className="sessionsTab" />Sessions</Tab>
+    <Tab><FaUser size={25} className="peopleTab" />People</Tab> */}
+            <Tab>Project</Tab>
+            <Tab>Sessions</Tab>
+            <Tab>People</Tab>
+          </TabList>
+          <TabPanel>
+            project
+  </TabPanel>
+
+          <TabPanel>
+            <SessionsPage />
+          </TabPanel>
+          <TabPanel>
+            people
+  </TabPanel>
+        </Tabs>
       </div>
     );
   }
