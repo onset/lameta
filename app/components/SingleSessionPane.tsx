67e6f30e1c2d3da68c2427e9896297dac70c4abb
John Hatton <hattonjohn@gmail.com>
2017-11-25 15:09:28 -0700

    Custom tab sets for each file type

--- app/components/SingleSessionPane.tsx
+++ app/components/SingleSessionPane.tsx
@@ -4,8 +4,9 @@ import { default as SessionForm } from "./SessionForm";
 import SessionFileList from "./SessionFileList";
 import { Session } from "../model/Session";
 import { observer } from "mobx-react";
-import * as path from "path";
+import * as Path from "path";
 import PropertyTable from "./PropertyTable";
+import { ComponentFile } from "../model/ComponentFile";
 const styles = require("./Sessions.scss");
 
 export interface IProps {
@@ -18,55 +19,91 @@ export class SingleSessionPane extends React.Component<IProps> {
 
   constructor(props: IProps) {
     super(props);
+    this.getTabs = this.getTabs.bind(this);
   }
 
   public render() {
     //console.log("Render SSPane:" + this.props.session.title.default());
-    const fullPath: string = path.join(
+    const fullPath: string = Path.join(
       this.props.session.directory,
       this.props.session.selectedFile.name
     );
 
-    const typesToTabs: { [type: string]: any } = {
-      Session: <SessionForm session={this.props.session} />,
-      Image: <img src={fullPath} />,
-      Audio: (
-        <audio controls>
-          <source src={fullPath} />
-        </audio>
-      )
-    };
-    this.filetypeSpecificTab =
-      typesToTabs[this.props.session.selectedFile.type];
-
-    console.log(
-      `Render SSPane: ${this.props.session.title.default()}   ${
-        this.filetypeSpecificTab.type
-      }    ${this.props.session.selectedFile.name}`
-    );
     return (
       <div className={styles.filePane}>
         <h3 className={styles.paneTitle}>
           {this.props.session.title.default()}
         </h3>
-        {<SessionFileList session={this.props.session} />}
-
-        <Tabs>
-          <TabList>
-            <Tab>{this.props.session.selectedFile.type}</Tab>
-            <Tab>Properties</Tab>
-            <Tab>Contributors</Tab>
-            <Tab>Notes</Tab>
-          </TabList>
-          <TabPanel>{this.filetypeSpecificTab}</TabPanel>
-          <TabPanel>
-            <PropertyTable file={this.props.session.selectedFile} />
-          </TabPanel>
-          <TabPanel>aaa</TabPanel>
-          <TabPanel>aaa</TabPanel>
-        </Tabs>
+        <SessionFileList session={this.props.session} />
+        {this.getTabs(this.props.session.selectedFile, fullPath)}
       </div>
     );
+  }
+
+  private getTabs(file: ComponentFile, path: string) {
+    if (!file) {
+      return <h3>none</h3>;
+    }
+
+    switch (file.type) {
+      case "Session":
+        return (
+          <Tabs>
+            <TabList>
+              <Tab>Session</Tab>
+              <Tab>Status &amp; Stages</Tab>
+              <Tab>Notes</Tab>
+            </TabList>
+            <TabPanel>
+              <SessionForm session={this.props.session} />
+            </TabPanel>
+            <TabPanel>todo</TabPanel>
+            <TabPanel>todo</TabPanel>
+          </Tabs>
+        );
+      case "Audio":
+        return (
+          <Tabs>
+            <TabList>
+              <Tab>Audio</Tab>
+              <Tab>Properties</Tab>
+              <Tab>Contributors</Tab>
+              <Tab>Notes</Tab>
+            </TabList>
+            <TabPanel>
+              <audio controls>
+                <source src={path} />
+              </audio>
+            </TabPanel>
+            <TabPanel>
+              <PropertyTable file={file} />
+            </TabPanel>
+            <TabPanel>todo</TabPanel>
+            <TabPanel>todo</TabPanel>
+          </Tabs>
+        );
+      case "Image":
+        return (
+          <Tabs>
+            <TabList>
+              <Tab>Image</Tab>
+              <Tab>Properties</Tab>
+              <Tab>Contributors</Tab>
+              <Tab>Notes</Tab>
+            </TabList>
+            <TabPanel>
+              <img src={path} />,
+            </TabPanel>
+            <TabPanel>
+              <PropertyTable file={file} />
+            </TabPanel>
+            <TabPanel>todo</TabPanel>
+            <TabPanel>todo</TabPanel>
+          </Tabs>
+        );
+      default:
+        return <h3>unknown type stuff</h3>;
+    }
   }
 }
 
