585291bfdaaca7e1902ccc07dc1dd299cc79e0f3
John Hatton <hattonjohn@gmail.com>
2017-12-27 07:55:04 -0700

    improve icons

--- app/components/ProjectIcon.tsx
+++ app/components/ProjectIcon.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export class ProjectIcon extends React.Component {
   public render() {
     return (
-      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB0AAAAYCAYAAAAGXva8AAAA5klEQVRIiWNgYGCQlhfju6cjJ/ICG1aXEnzCz87iykBl4NcVafPj2bSU/9jwnqqg/0K8HA+UxAW2UIrFBDiLibL04aSk/8ebwqmCo6xV3zMwMFgQtBQZ35mQ8P96TyzZuDXM6gcDA4Mf0Zb6myj/9zFU/O9rRD6205D6JyXIc4QoS8+2Rf4PNVclGBLEYAM50TcMcsK8N+w1pf/hc6GfsdL/Y41hVLHUSF7sDYORvNgbahg2aumopaOWjlo6aumopYPEUmMF0ef0tNRYQfQ5g7wwz35LVfFnZioSb2iOlcWfK4nx7wUA7Ji3usTrSNgAAAAASUVORK5CYII=" />
+      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB0AAAAYCAYAAAAGXva8AAACL0lEQVRIiWNgGAW0BO3uChumBevcxYbbPRQW0MTSif4aV59NS/mPDU8K1HrR4al0iVzc4iE7B6ul/f6q53FZSinelG37ttRAwADD0l4f1ZOkGHSnP/b/mjTr/yuSLQniWWG6/xpdpPc0u8ouaHaVXdDsIj2vwU7SjqHLU+kIKZauTbf+f6kt7P/9CXEk43v9cf/7/dUeMbS6y++D+6Iv9v/cSIP/s8J1seMI/f+rU60oCvJ5UYaPGNrcFDc/npL4/9m0lP8Hyj3/H632oUn8wvD0MN37DM1usgvv9MX+fzYt5f/6DNv/1zojaWrpBD+1iwxt7vK91zoi/j+blvJ/QYzx/ydTk2lqaYeH0mGGZgeZ8nNNIf+fTUv5Pz/GkKYW3uuP+9/mLr+ModJONP5ErR/UUiOaWnq2Ieh/o71kIUOBBZ/n/jKP33f6Yv+vTLGkqaU7C12+FBoJ2jLkmQpZ7chz+ny6PuD/rmJXmlq6MM7oSYkVjxhDrjmP1rp029c7C13+n20Ioqmlk4I0bzEwMDAwZFgKSy+JN322Itni/73+OJpa2uendpqBgYGBoUScgXtuhN4jWieip1OT/3d4Ku6CF/rTgrTu0zq7XOuM/N/qJjcJbumkII1bC2ONaWrp0UrvfxWW4pFwS7u8lW9tzLKlqaXrM+3e5FgK6sItbXCRO3+o3Jumls4M03kYasnACbe03E7UrtdX5cWUYO1XVMdB2o+nBevc7fRQ2gqzDwCfMMeUV+o27QAAAABJRU5ErkJggg==" />
     );
   }
 }
