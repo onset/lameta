1b298ef1f76bafecc9d7db182d294dd9d433179c
John Hatton <hattonjohn@gmail.com>
2018-04-05 08:28:34 -0600

    Convert Property Table to react-table

--- app/components/PropertyTable.tsx
+++ app/components/PropertyTable.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Table, Column, Cell } from "@blueprintjs/table";
+import ReactTable from "react-table";
 import { observer } from "mobx-react";
 import { Field, FieldType } from "../model/field/Field";
 import { FieldSet } from "../model/field/FieldSet";
@@ -15,36 +15,38 @@ interface IState {
 
 @observer
 export default class PropertyTable extends React.Component<IProps, IState> {
-  private getFieldNameCell(rowIndex: number) {
-    const v = this.props.fields.values();
-    const f: Field = v[rowIndex];
-    return <Cell>{f.englishLabel}</Cell>;
-  }
-  private getFieldValueCell(rowIndex: number) {
-    const p = this.props.fields.values()[rowIndex];
-    if (p.type === FieldType.Text) {
-      return <Cell>{p.toString()}</Cell>;
-    }
-    if (p.type === FieldType.Date) {
-      return <Cell>{p.asLocaleDateString()}</Cell>;
-    }
-
-    return <Cell />;
-  }
   public render() {
+    const columns = [
+      {
+        id: "property",
+        Header: "Property",
+        width: 120,
+        accessor: (f: Field) => {
+          return f.englishLabel;
+        }
+      },
+      {
+        id: "value",
+        Header: "Value",
+        width: 200,
+        accessor: (f: Field) => {
+          if (f.type === FieldType.Text) {
+            return f.toString();
+          }
+          if (f.type === FieldType.Date) {
+            return f.asLocaleDateString();
+          }
+          return "ERROR";
+        }
+      }
+    ];
+
     return (
-      <div>
-        {/* TODO: change to react-table so we can drop this dependency on blueprintjs/table (and make editable cells) */}
-        <Table
-          numRows={this.props.fields.keys().length}
-          isRowHeaderShown={false}
-          allowMultipleSelection={false}
-          columnWidths={[80, 200]}
-        >
-          <Column name="Property" renderCell={r => this.getFieldNameCell(r)} />
-          <Column name="Value" renderCell={r => this.getFieldValueCell(r)} />
-        </Table>
-      </div>
+      <ReactTable
+        showPagination={false}
+        data={this.props.fields.values()}
+        columns={columns}
+      />
     );
   }
 }
