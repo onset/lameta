865787aef6d6d86c707089b78b6c0d1c17c5fa9a
John Hatton <hattonjohn@gmail.com>
2018-04-07 11:09:31 -0600

    Restore the preservation of state when doing hot module reloading

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import HomePage from "./HomePage";
-export default class App extends React.Component {
+import { hot } from "react-hot-loader";
+export class App extends React.Component {
   public render() {
     return (
       <div id="app">
@@ -9,3 +10,9 @@ export default class App extends React.Component {
     );
   }
 }
+
+// This restores hot module replacement *while preserving state*, which was lost when
+// react-hot-loader dropped its react-hot-loader/webpack loader
+// See https://github.com/gaearon/react-hot-loader/issues/923
+declare const module: any;
+export default hot(module)(App);
