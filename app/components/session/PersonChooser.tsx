096a473cdd2f5b1dfaeffcb6502ee4a4f1dc2ff3
John Hatton <hattonjohn@gmail.com>
2018-05-05 08:06:00 -0600

    Allow contributions to show unknown user names

--- app/components/session/PersonChooser.tsx
+++ app/components/session/PersonChooser.tsx
@@ -21,17 +21,31 @@ export default class PersonChooser extends React.Component<IProps> {
   }
 
   public render() {
+    //console.log("person name: " + JSON.stringify(this.props.name));
     const choices = this.props.getPeopleNames().map(c => {
       return new Object({
         value: c,
         label: c
       });
     });
+    /* Question: Should we allow contributors that we don't have a "Person" (Actor) record for?
+      SayMore Classic does as of 2017. So we have to support data incoming that way. 
+      But it's a separate question of whether we want to allow new contributors that don't have
+      people records. */
+    if (
+      this.props.name &&
+      choices.filter((c: any) => c.label === this.props.name).length === 0
+    ) {
+      //console.log(`${this.props.name} was not in the list of people choices.`);
+      choices.push(
+        new Object({ value: this.props.name, label: this.props.name })
+      );
+    }
     return (
-      <ReactSelect
+      //<ReactSelect <-- if we didn't want to allow new
+      <Creatable
         name={this.props.name}
         value={this.props.name}
-        //onChange={v => this.props.onChange(v ? v[0].value : "")}
         onChange={v => {
           const s: string = v as any;
           this.props.onChange(s ? s : "");
