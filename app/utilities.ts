e63968c0a9613aeafc5a62b8ed8d95cb3c8e5a0d
John Hatton <hattonjohn@gmail.com>
2018-05-17 14:27:22 -0600

    If file isn't found locate can just return empty string.

--- app/utilities.ts
+++ app/utilities.ts
@@ -47,16 +47,28 @@ export function locate(path: string): string {
   //  windows from installed package: assets & sample data are one level up from apppath:
   //                        "C:\Users\me\AppData\Local\Programs\myapp\resources\app.asar"
   // mac from a package: <somewhere>"/my.app/Contents/Resources/app.asar"
-
   // windows from (uninstalled) package: assets & sample data are one level up from apppath:
   //                       <somewhere>"\myapp\release\win-unpacked\resources\app.asar"
+
+  // In most cases, our extra files (assets/, sample data/) are all on the file system, and
+  // the working directory is set such that they are the immediate children.
+
+  // However when running from a packaged up mac app, the whole file tree has been encased in a single file,
+  // and the working directory is who-knows-where; it cannot point into that archive.
+  // However electron manages to intercept filesytem calls so you can point into the insides of the app.
+  // In this case, apppPath will be <somewhere>/my.app/Contents/Resources/app.asar
+  // But the root we want, to be the same as the other situations, is in my.app/contents.
 
   if (appPath.indexOf("default_app.asar") < 0) {
     adjustment = Path.normalize(appPath + "/../..");
   }
-  console.log(`locate(${path})  adjustment= ${adjustment}`);
-  const result = fs.realpathSync(Path.join(adjustment, path));
-  console.log(`locate(${path})  result= ${result}`);
-
-  return result;
+  //console.log(`locate(${path})  adjustment= ${adjustment}`);
+  try {
+    const result = fs.realpathSync(Path.join(adjustment, path));
+    //console.log(`locate(${path})  result= ${result}`);
+    return result;
+  } catch (err) {
+    console.error("Could not find: " + Path.join(adjustment, path));
+    return "";
+  }
 }
