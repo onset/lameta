87b1cf8caca3f976767b42ba8fc47cc63d2ebabd
John Hatton <hattonjohn@gmail.com>
2018-09-11 18:56:07 -0600

    fix test runner

--- test/runTests.js
+++ test/runTests.js
@@ -2,6 +2,7 @@ const spawn = require("cross-spawn");
 const path = require("path");
 
 let pattern;
+
 switch (process.argv[2]) {
   case "e2e":
     pattern = "test/e2e/.+\\.spec\\.tsx?";
@@ -13,18 +14,19 @@ switch (process.argv[2]) {
     pattern = ".(spec|test)\\.tsx?";
 }
 
-let cliSwitch = "";
+let cliSwitch = undefined;
 if (process.argv[2] && process.argv[2].startsWith("--")) {
   cliSwitch = process.argv[2];
 } else if (process.argv[3] && process.argv[3].startsWith("--")) {
   cliSwitch = process.argv[3];
 }
+
 const result = spawn.sync(
   path.normalize("./node_modules/.bin/jest"),
   // process.argv[2] && process.argv[2].startsWith("--")
   //   ? [pattern, process.argv[2]]
   //   : [pattern],
-  [pattern, cliSwitch],
+  cliSwitch ? [pattern, cliSwitch] : [pattern],
   { stdio: "inherit" }
 );
 
