3085ea6cce88d4fceff5ed638c909104ef51d16a
John Hatton <hattonjohn@gmail.com>
2019-09-27 08:02:13 -0600

    WIP macOS notarization

--- build/notarize.js
+++ build/notarize.js
@@ -1,5 +1,9 @@
 // notarize the mac version
 
+// to test notarization by hand, do something like:
+xcrun altool --verbose --notarize-app -f "SayMore X-0.7.5-mac.zip" --primary-bundle-id "io.github.saymore.saymore-x" -u "<your apple id>" -p "@keychain:altool.pw"
+// Review: what's the actual best version of this to be notarizing? The Zip? The raw app?
+
 const { notarize } = require("electron-notarize");
 
 exports.default = async function notarizing(context) {
@@ -14,7 +18,9 @@ exports.default = async function notarizing(context) {
     appBundleId: "io.github.saymore.saymore-x",
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS
+    // this is an app-specific password you get from appid.apple.com, then put that value in "altool.pw" 
+    // in the keychain. I named mine "altoo.pw" because if you do this by hand, that's the tool you're talking to.
+    appleIdPassword: "@keychain:altool.pw"
   };
   console.log(`notarize(${JSON.stringify(params)})`);
   return await notarize(params);
