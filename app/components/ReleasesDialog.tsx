db7e0155c85a2980c49ef1ea34105f9cbe5b7d1b
John Hatton <hattonjohn@gmail.com>
2020-12-28 09:42:35 -0700

    Tweak release recommendation algortihm

--- app/components/ReleasesDialog.tsx
+++ app/components/ReleasesDialog.tsx
@@ -23,6 +23,8 @@ type Mode =
   | "just-show-release-notes"
   | "closed";
 
+type Channel = "alpha" | "beta" | "release";
+
 interface IGithubRelease {
   assets: { browser_download_url: string }[];
   body: string;
@@ -44,14 +46,29 @@ export const ReleasesDialog: React.FunctionComponent<{}> = (props) => {
   const [allReleases, setAllReleases] = React.useState<IGithubRelease[]>([]);
   const [showNewReleasesOnly, setShowNewReleasesOnly] = React.useState(true);
 
+  const channelsToRecommend = getChannelsToRecommend();
   //checkForUpdates = (callback: (haveUpdates: boolean) => void) => {
   checkForUpdates = () => {
     axios
       .get("https://api.github.com/repos/onset/lameta/releases")
       .then((result) => {
-        const all = sortReleases(result.data);
+        const all = sortReleases(result.data).filter((r) => r.draft === false);
+
+        // const all = [
+        //   {
+        //     assets: [],
+        //     body: "foo",
+        //     created_at: "",
+        //     draft: false,
+        //     html_url: "",
+        //     name: "foo",
+        //     prerelease: false,
+        //     published_at: "",
+        //     tag_name: "0.8.0-beta.1",
+        //   },
+        // ];
         setAllReleases(all);
-        if (getNewerReleases(all).length) {
+        if (getNewerReleases(all, channelsToRecommend).length) {
           NotifyUpdateAvailable(() => setMode("update-available"));
         }
       })
@@ -73,7 +90,7 @@ export const ReleasesDialog: React.FunctionComponent<{}> = (props) => {
   React.useEffect(() => checkForUpdates(), []);
 
   const releasesToShow = showNewReleasesOnly
-    ? getNewerReleases(allReleases)
+    ? getNewerReleases(allReleases, channelsToRecommend)
     : allReleases;
 
   //const notes = notesArray ? notesArray!.map((n) => n.note).join("<br/>") : "";
@@ -226,24 +243,35 @@ function getDownloadUrl(release: any) {
   }
 }
 
-function getChannel(version: string): "alpha" | "beta" | "release" {
+function getChannel(version: string): Channel {
   if (_.includes(version, "alpha")) return "alpha";
   if (_.includes(version, "beta")) return "beta";
   return "release";
 }
 
-function getNewerReleases(releasesNewestFirst: Array<IGithubRelease>) {
-  const current = require("../package.json").version as string;
-  const releasesOnThisChannel = releasesNewestFirst.filter(
-    (r) => getChannel(r.tag_name) === getChannel(current)
-  );
-  const newerReleases = releasesOnThisChannel.filter(
-    (r) => Semver.compare(r.tag_name, current) > 0
-  );
-  return newerReleases;
+// this isn't ideal... once someone goes up to a release, they'll never be
+// informed about betas. To improve this, we will need some more UI so that
+// people can specify what channels they are interested in hearing about.
+function getChannelsToRecommend(): Channel[] {
+  const current = getChannel(require("../package.json").version as string);
+  if (current === "alpha") return ["alpha", "beta", "release"];
+  if (current === "beta") return ["beta", "release"];
+  return ["release"];
 }
 
-function sortReleases(releases: Array<IGithubRelease>) {
+function getNewerReleases(
+  releasesNewestFirst: IGithubRelease[],
+  channelsToInclude: Channel[]
+) {
+  const current = require("../package.json").version as string;
+  return releasesNewestFirst.filter(
+    (r) =>
+      channelsToInclude.includes(getChannel(r.tag_name)) &&
+      Semver.compare(r.tag_name, current) > 0
+  );
+}
+
+function sortReleases(releases: IGithubRelease[]) {
   try {
     // Here we call "coerce" to make it a bit more tolerant of accidents like an upper-case V or a missing 3rd digit.
     // But if we really screw up, an get an error here,  just give up on the sorting.
