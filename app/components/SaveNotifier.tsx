210a66b0d8d475b16f7b92aaeaf03fba9b475b46
Hatton <hattonjohn@gmail.com>
2022-12-16 09:58:54 -0700

    Stop Save Notifier from covering up buttons when hidden

--- app/components/SaveNotifier.tsx
+++ app/components/SaveNotifier.tsx
@@ -1,17 +1,22 @@
-import "./SaveNotifier.scss";
+// this engages a babel macro that does cool emotion stuff (like source maps). See https://emotion.sh/docs/babel-macros
+import css from "@emotion/css/macro";
+// these two lines make the css prop work on react elements
+import { jsx } from "@emotion/core";
+/** @jsx jsx */
+
 import { Trans } from "@lingui/macro";
 import * as React from "react";
 // tslint:disable-next-line: no-duplicate-imports
 import { useState, useEffect } from "react";
+import { lameta_blue } from "../containers/theme";
 
 let staticSetShowSaving: (b: boolean) => void;
 let staticSetFilename: (name: string) => void;
 let staticBeforeRename: (b: boolean) => void;
 
 export function ShowSavingNotifier(filename: string, beforeRename: boolean) {
-  //console.log("Called ShowSavingNotifier()");
   if (staticSetShowSaving) {
-    // console.log("Showing SavingNotifier()");
+    //console.log(`Showing SavingNotifier(${filename})`);
     staticSetFilename(filename);
     staticSetShowSaving!(true);
     staticBeforeRename(beforeRename);
@@ -35,7 +40,25 @@ export const SaveNotifier: React.FunctionComponent<{}> = (props) => {
   }, [showSaving]);
   // in e2e testing, this covers up buttons that we need to click, maybe because the fading is just done by changing opacity?
   return process.env.E2E ? null : (
-    <div id="saveNotifier" className={showSaving ? "show" : "hide"}>
+    <div
+      id="saveNotifier"
+      css={css`
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: white;
+        background-color: ${lameta_blue};
+        padding: 5px;
+        z-index: -1;
+        opacity: 0;
+        // after fading out, also hide it so it doesn't cover up buttons
+        ${!showSaving &&
+        "z-index:-1; opacity: 0; transition: z-index 0.5s step-end, opacity 0.5s ease-in;"}
+        //  https://css-tricks.com/ease-out-in-ease-in-out/ "ease-out" refers to the acceleration curve
+        ${showSaving &&
+        "z-index: 100; opacity: 1; transition: opacity 0.5s ease-out;"}
+      `}
+    >
       {beforeRename && (
         <div>
           <Trans>Saving before rename...</Trans>
