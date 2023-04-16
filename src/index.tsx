7ba3abcb8b4534be4c94165c6480ff564ee8b330
Hatton <hattonjohn@gmail.com>
2023-04-16 16:21:41 -0600

    help compiler with types coming out of process

--- src/index.tsx
+++ src/index.tsx
@@ -21,10 +21,10 @@ type TestEnvironment = {
 export function getTestEnvironment(): TestEnvironment {
   // Indexing instead of, e.g., `process.env` because vite replaces that with ({}). Bad vite!
   const e = {
-    E2E: process["env"]["E2E"],
+    E2E: !!process["env"]["E2E"],
     E2E_USER_SETTINGS_STORE_NAME:
-      process["env"]["E2E_USER_SETTINGS_STORE_NAME"],
-    E2ERoot: process["env"]["E2ERoot"]
+      process["env"]["E2E_USER_SETTINGS_STORE_NAME"] || "",
+    E2ERoot: process["env"]["E2ERoot"] || ""
   };
   //console.log("getTestEnvironment() = ", JSON.stringify(e, null, 2));
   return e;
