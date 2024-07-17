06153171f7ecad904dadc0d47c11a61127302dda
Hatton <hattonjohn@gmail.com>
2024-07-17 08:38:28 -0600

    Fix user settings file name

--- src/other/UserSettings.ts
+++ src/other/UserSettings.ts
@@ -5,6 +5,9 @@ import { observable, computed, makeObservable } from "mobx";
 import React from "react";
 import pkg from "package.json";
 import { getTestEnvironment } from "../getTestEnvironment";
+import { app, process } from "@electron/remote";
+import Path from "path";
+import fs from "fs-extra";
 
 class FakeStore {
   private values = {};
@@ -48,14 +51,35 @@ export class UserSettings {
     // Also, if we copy getTestEnvironment() into this file, it doesn't work.
     // Only works if we call from the other file. As if that file is in a different module with different acces to process.env.
     // We know that vite is vicious about breaking process.env, so it has to be something to do with that.
-    const E2E_USER_SETTINGS_STORE_NAME = getTestEnvironment()
-      .E2E_USER_SETTINGS_STORE_NAME;
+    const E2E_USER_SETTINGS_STORE_NAME =
+      getTestEnvironment().E2E_USER_SETTINGS_STORE_NAME;
 
+    let name = "lameta-user-settings";
+    if (E2E_USER_SETTINGS_STORE_NAME?.length > 0) {
+      name = E2E_USER_SETTINGS_STORE_NAME;
+    } else {
+      // at some point during 2.3 beta, we broke the file name here,
+      // leaving it named ".json"
+      // So here we fix it up if we find that.
+      const brokenPath = Path.join(app.getPath("userData"), ".json");
+      if (fs.existsSync(brokenPath)) {
+        const correctPath = Path.join(app.getPath("userData"), name + ".json");
+        try {
+          if (!fs.existsSync(correctPath)) {
+            fs.renameSync(brokenPath, correctPath);
+          } else {
+            fs.rmSync(brokenPath); // we have two files, bad and good names. Remove incorrectly named one (note, it could be the more recent, ah well)
+          }
+        } catch (e) {
+          // ah well
+        }
+      }
+    }
     this.store =
       process.env.NODE_ENV === "test" || E2E_USER_SETTINGS_STORE_NAME === "none" // like we're running for the first time
         ? new FakeStore()
         : new Store({
-            name: E2E_USER_SETTINGS_STORE_NAME ?? "lameta-user-settings"
+            name: name
           });
     this.sendErrors = process.env.NODE_ENV === "production"; // developer has a menu that can toggle this
 
