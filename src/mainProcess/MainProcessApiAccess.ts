bcde5ece32d27ea6128573fa12dddeb1fd3d5435
Hatton <hattonjohn@gmail.com>
2025-07-30 14:42:25 -0600

    pesky type error

--- src/mainProcess/MainProcessApiAccess.ts
+++ src/mainProcess/MainProcessApiAccess.ts
@@ -7,16 +7,22 @@ import call from "electron-call";
 // notice, we're importing type info only. The code won't be loaded in the render process, won't be available in browser debugger
 import type { MainProcessApi } from "./MainProcessApi";
 
-let mainProcessApi: MainProcessApi;
+// Define the public API interface that will be available through electron-call
+type MainProcessApiPublic = Pick<
+  MainProcessApi,
+  "trashItem" | "validateImdiAsync" | "findInPage" | "stopFindInPage"
+>;
+
+let mainProcessApi: MainProcessApiPublic;
 
 // This shouldn't be needed, since there is a mock in __mocks__. But that isn't working at the moment.
 if (process.env.VITEST_POOL_ID && process.env.VITEST_WORKER_ID) {
   import("./__mocks__/MainProcessApiAccess").then((m) => {
     // NB: don't try to use await here, it is currently breaking vite build
-    mainProcessApi = (m as unknown) as any;
+    mainProcessApi = m as unknown as any;
   });
 } else {
-  mainProcessApi = call.use<MainProcessApi>("MainProcessApi");
+  mainProcessApi = call.use<MainProcessApiPublic>("MainProcessApi");
 }
 
 export { mainProcessApi };
